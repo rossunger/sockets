86bfe81406ac5e1bac70e67c70b48798b95f9ee5
rossunger <ross93@gmail.com>
2024-07-27 22:21:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+# Create a Socket.IO server
+sio = socketio.Server()
+
+# Create a WSGI application
+application = socketio.WSGIApp(sio,app)
+
+
+users = []
+# Handle a message event
+@sio.event
+def message(sid, data):
+    print(f'Message from {sid}: {data}')
+    sio.send(f'Server received: {data}')
+
+# Handle a connect event
+@sio.event
+def connect(sid, environ):
+    users.append(sid)
+    print('Client connected: ', sid, " 1 of ", len(users))
+    sio.send(f'{sid} connected to server')
+
+# Handle a disconnect event
+@sio.event
+def disconnect(sid):
+    del users[users.index(sid)]
+    sio.send(f'{sid} left')
+    print('Client disconnected: ', sid)
+
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
